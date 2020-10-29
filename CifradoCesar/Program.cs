a26409d4ec3188beffc20ca10c6100880e4e554a
Miguel <miguel.cuadrado@seidor.com>
2020-10-28 22:26:51 -0500

    primer commit

---
+++ CifradoCesar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+
+namespace CifradoCesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            do {
+                Console.WriteLine("Escoga una opcion: \n 1.Encriptar \n 2.Desencriptar \n 3.Salir");
+                opt = Console.ReadLine();
+
+                switch (opt)
+                {
+                    case "1":
+
+                        Console.WriteLine("Escriba mensaje");
+                        string msg = Console.ReadLine().ToLower();
+
+                        if (!msg.Equals(string.Empty))
+                        {
+                            string res = Commons.Encrypt(msg);
+                            Console.WriteLine("Mensaje encriptado " + res);
+                        }
+                        break;
+                    case "2":
+
+                        Console.WriteLine("Ingrese mensaje encriptado");
+                        string msgEncript = Console.ReadLine().ToLower();
+
+                        if (!msgEncript.Equals(string.Empty))
+                        {
+                            string res = Commons.DesEncrypt(msgEncript);
+                            Console.WriteLine("Mensaje desencriptado " + res);
+                        }
+
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                }
+
+                Console.WriteLine("Desea continuar: \n 1.Si \n 2.No");
+                opt = Console.ReadLine();
+
+            } while (opt.Equals("1"));
+        }
+    }
+}
+
+
---
+++ WCFService/Service1.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCFService.Model;
+
+namespace WCFService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public int codeResult;
+       
+        IEnumerable<ROL> IService1.GetRolbyId(int id)
+        {
+            try
+            {
+                using (var context = new Model.BANKEntities())
+                {
+                    var rol = new int[] { id };
+                    return context.ROL.OrderBy(p => p.id).ToList().Where(p => rol.Contains(p.id)).Select(p => new ROL() { id = p.id, rol1 = p.rol1 });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
